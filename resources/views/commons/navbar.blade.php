d7026fab476053c6363e0a24218a536db1631604
taro.kirameki <kazuya.19980817@gmail.com>
2020-07-16 11:36:55 +0000

    user login

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -10,13 +10,23 @@
         <div class="collapse navbar-collapse" id="nav-bar">
         　　<ul class="navbar-nav mr-auto"></ul>
         　　<ul class="navbar-nav">
-                <li class="nav-item">{!! link_to_route('tasks.create', '新規作成', [], ['class' => 'nav-link']) !!}</li>
-            </ul>
-             <ul class="nav navbar-nav navbar-right">
-                {{-- ユーザ登録ページへのリンク --}}
-                <li>{!! link_to_route('signup.get', 'Signup', [], ['class' => 'nav-link']) !!}</li>
-                {{-- ログインページへのリンク --}}
-                <li><a href="#">Login</a></li>
+        　　    @if (Auth::check()
+        　　      <li class="nav-item dropdown">
+                    <ul class="dropdown-menu dropdown-menu-right">
+                        {{-- ログアウトへのリンク --}}
+                        <li class="dropdown-item">{!! link_to_route('logout.get', 'Logout') !!}</li>
+                    </ul>
+                </li>
+              @else
+                <ul class="nav navbar-nav navbar-right">
+                    {{-- ユーザ登録ページへのリンク --}}
+                    <li>{!! link_to_route('signup.get', 'Signup', [], ['class' => 'nav-link']) !!}</li>
+                    {{-- ログインページへのリンク --}}
+                    <li><a href="#">Login</a></li>
+                </ul>
+              @endif
+                
+        　　  　  <li class="nav-item">{!! link_to_route('tasks.create', '新規作成', [], ['class' => 'nav-link']) !!}</li>
             </ul>
     　　</div>
     </nav>
