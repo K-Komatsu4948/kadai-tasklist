574de06dd329ac1d4f6748a543f99e50df3a3562
taro.kirameki <kazuya.19980817@gmail.com>
2020-07-18 07:57:37 +0000

    CRUD messages

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -144,6 +144,7 @@ class TasksController extends Controller
      */
     public function destroy($id)
     {
+        $task = Task::findOrFail($id);
         if (\Auth::id() === $task->user_id) {
             $task->delete();
             
